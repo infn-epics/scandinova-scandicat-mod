419b050121f1b9516a359d0c1b423dddac002635
Andrea Michelotti <andrea.michelotti@gmail.com>
2026-01-30 09:06:06 +0200

    added max hopr values, alarms on pump and changed nA to ionpump

--- iocBoot/iocscandinova-rfmod/st-eli.cmd
+++ iocBoot/iocscandinova-rfmod/st-eli.cmd
@@ -41,9 +41,15 @@ epicsEnvSet("RFMOD_TCP_PORT",   "502")
 #asynSetTraceMask("$(RFMOD_ASYNPORT)_TCP_PROT_ID", 0, 11)
 
 ## Load record instances
-dbLoadRecords("../../db/scandinova-rfmod.db","RFMOD_ASYNPORT=RFMod_1, PREFIX=MODHEL01")
-dbLoadRecords("../../db/scandinova-rfmod.db","RFMOD_ASYNPORT=RFMod_2, PREFIX=MODHEL02")
-dbLoadRecords("../../db/scandinova-rfmod.db","RFMOD_ASYNPORT=RFMod_3, PREFIX=MODHEL03")
+## You can override default macros per device:
+##   HOPR macros: CCPS_V_SP_HOPR (default=1500), PLS_WDTH_SP_HOPR (default=10), FILPS_CURR_SP_HOPR (default=30)
+##   Alarm macros: IONPMP_HIGH (default=8000), IONPMP_HIHI (default=9000)
+dbLoadRecords("../../db/scandinova-rfmod.db","RFMOD_ASYNPORT=RFMod_1, PREFIX=MODHEL01, IONPMP_HIGH=7500, IONPMP_HIHI=8500, CCPS_V_SP_HOPR=1270")
+dbLoadRecords("../../db/scandinova-rfmod.db","RFMOD_ASYNPORT=RFMod_2, PREFIX=MODHEL02, IONPMP_HIGH=7500, IONPMP_HIHI=8500, CCPS_V_SP_HOPR=1270")
+dbLoadRecords("../../db/scandinova-rfmod.db","RFMOD_ASYNPORT=RFMod_3, PREFIX=MODHEL03, IONPMP_HIGH=7500, IONPMP_HIHI=8500, CCPS_V_SP_HOPR=1270")
+
+## Example with custom values for a specific device:
+#dbLoadRecords("../../db/scandinova-rfmod.db","RFMOD_ASYNPORT=RFMod_1, PREFIX=MODHEL01, IONPMP_HIGH=7500, IONPMP_HIHI=8500, CCPS_V_SP_HOPR=2000")
 
 # dbLoadRecords("../../db/scandinova-rfmod.db","RFMOD_ASYNPORT=RFMod_3, PREFIX=MODHEL02")
 
